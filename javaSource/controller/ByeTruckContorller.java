cedfae5cc53dbfa2df2068e3a729474868343d5f
yuljune <yoojune0213@gmail.com>
2017-10-14 20:02:23 +0900

    controller

---
+++ javaSource/controller/ByeTruckContorller.java
@@ -0,0 +1,53 @@
+package com.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mvc.dao.MemberDAO;
+import com.mvc.dao.TruckDAO;
+
+
+@WebServlet("/byetruck")
+public class ByeTruckContorller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public ByeTruckContorller() {
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Process(request,response);
+	}
+
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Process(request,response);
+	}
+	private void Process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("user_id");
+		System.out.println(id);
+		
+		TruckDAO dao = new TruckDAO();
+		
+		String msg = "삭제에 실패 했습니다.";
+		if(dao.truckdelete(id) ==1){
+			msg = "삭제에 성공 했습니다.";
+		}
+		//response.sendRedirect("admin");
+		request.setAttribute("list", msg);
+		RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
+		dis.forward(request, response);
+		
+	}
+
+}
